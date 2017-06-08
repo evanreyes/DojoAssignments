e0ddbd77a3007f5044c3819ca929f3c377692ec4
evanreyes <evanreyes@me.com>
2017-06-08 14:03:04 -0700

    Commit after finishing Random Word Generator

---
+++ python/week5/3_wednesday/random_word_generator/apps/word_generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+import random, string
+
+def check_attempts():
+    if 'attempts' in request.session == True:
+        return redirect('/')
+    else:
+        request.session['attempts'] = 1
+
+def index(request):
+    if request.method == "POST":
+        gen_word = ''.join([random.choice(string.ascii_uppercase + string.digits) for i in range(14)])
+        if 'attempts' in request.session:
+            request.session['attempts'] += 1
+        else:
+            request.session['attempts'] = 1
+
+        random_word = {
+            'word': gen_word
+        }
+
+        return render(request, 'word_generator/index.html', random_word)
+    else:
+        return render(request, 'word_generator/index.html')
+
+def reset(request):
+    request.session['attempts'] = 0
+    return redirect('/')

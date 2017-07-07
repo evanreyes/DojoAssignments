cd93a369eb86c7e5a1e3cffec3093d0544f3d638
evanreyes <evanreyes@me.com>
2017-07-07 12:38:53 -0700

    7/7

--- mean/week1/1_monday/js_intermediate/main.js
+++ mean/week1/1_monday/js_intermediate/main.js
@@ -1,2 +1,10 @@
 // Part I
 // Create a function called star_string() that takes a number and returns a string of that many *. For example:
+function star_string(num) {
+    let stars = '';                     // We start by creating an empty string that we will populate with stars
+    for (var i = 1; i <= num; i++) {    // This for loop will create a star based on the num argument we pass in
+        stars += '*';                   // This will concatenate one star to the same string each time we increment toward num
+    }
+    console.log(stars);                 // Show me the stars!
+}
+star_string(10)                         // Run it!

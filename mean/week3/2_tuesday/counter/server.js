e6d2b28f255228a74be17cd5ceae16f484cbd68b
evanreyes <evanreyes@me.com>
2017-07-17 14:41:42 -0700

    7/17

---
+++ mean/week3/2_tuesday/counter/server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var session = require('express-session');
+var path = require("path");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(session({secret: 'codingdojorocks'}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/static"));
+
+// Declaring a global variable for our running count
+let count = 0
+
+// On page load, increment the count by 1, and pass that variable into our EJS template
+app.get('/', function(req, res) {
+    count += 1;
+    res.render("index", {count: count});
+})
+
+// Route to handle our "add two" button, which will insert an extra incrementation
+// Since the root route already adds one, this will have the effect of adding two to the count
+app.post('/plus_two', function(req, res) {
+    count += 1;
+    res.redirect('/');
+})
+
+// Clicking the reset button will move count to zero, but then return it to 1 upon hitting the root route
+app.post('/reset', function(req, res) {
+    count = 0;
+    res.redirect('/');
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
